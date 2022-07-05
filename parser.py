901cb8914f82324c849c0795fe16d063650589a3
Alexander Morozov <reverse43@gmail.com>
2022-07-05 14:11:12 +0300

    Save changes

--- parser.py
+++ parser.py
@@ -1,96 +1,102 @@
+import asyncio
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+
+from db.base import database
+from db.models import straight_sofas
 
 base_url = "https://azbykamebeli.ru"
 url = "https://azbykamebeli.ru/catalog/0000057/"
 
 
-# Not used, but maybe helpful if there will be more or less than 25 pages
-def get_number_of_pages(session) -> int:
-    response = session.get(url)
-    soup = BeautifulSoup(response.text, "lxml")
-    number_of_pages = int(
-        soup.select_one(
-            "ul.pagination li.page-item:nth-child(7) a.page-link"
-        ).get_text()
-    )
-    return number_of_pages
+async def write_to_db(data) -> None:
+    query = straight_sofas.insert()
+    await database.execute_many(query=query, values=data)
 
 
-def write_to_db(data) -> None:
-    pass
+def get_sku(item):
+    return item.select_one("div.item__description > div.d-flex.justify-content-between > small.text-muted.f-XS").get_text().split(": ")[1]
 
 
-def parse_data(session) -> None:
-    for page_number in range(1, 26):
+async def parse_data(session) -> None:
+    for page_number in range(2, 3):
+        print(f"Page {page_number}/25")
         parsed_page_data = []
         page_url = f"https://azbykamebeli.ru/catalog/0000057/?page={page_number}"
-        response = session.get(page_url)
-        soup = BeautifulSoup(response.text, "lxml")
-        all_item_links = soup.select("div.items-list div.row div.item__title.h4 a")
+        # response = session.get(page_url)
+        # soup = BeautifulSoup(response.text, "lxml")
+        with open("page.html", encoding="utf-8") as f:
+            response = f.read()
+        soup = BeautifulSoup(response, "lxml")
         sleep(2)
-
-        for item in all_item_links:
-            item_link = base_url + item["href"]
-            response = session.get(item_link)
-            soup = BeautifulSoup(response.text, "lxml")
-
-            title = soup.select_one("div.container.type-page h1").get_text()
+        items = soup.select("div.items-list > div > div > div")
+        for item in items:
             sku = (
-                soup.select_one(
-                    "div.info-container div.align-self-start span:nth-child(1)"
+                item.select_one(
+                    "div.item__description > div.d-flex.justify-content-between > small.text-muted.f-XS"
                 )
                 .get_text()
                 .split(": ")[1]
             )
-            id_ = (
-                soup.select_one(
-                    "div.info-container div.align-self-start span:nth-child(2)"
-                )
-                .get_text()
-                .split(": ")[1]
+            item_id = int(
+                item.select_one(
+                    "div.items-list > div > div > div > div.item__description > div.item__title.h4 > a"
+                )["href"]
+                .split("/")[-1]
+                .split("=")[-1]
             )
-            full_price = int(
-                soup.select_one(
-                    "div.row div.col-12.col-md-8.d-flex.justify-content-between a.store-price.fake-link"
+            title_base = item.select_one(
+                "div.items-list > div > div > div > div.item__description > div.item__title.h4 > a > span"
+            ).get_text()
+            title_additional = item.select_one(
+                "div.items-list > div > div > div > div.item__description > p:last-child"
+            ).get_text()
+            if not title_additional:
+                title_additional = item.select_one(
+                    "div.items-list > div > div > div > div.item__description > p:nth-last-child(-n+2)"
+                ).get_text()
+            try:
+                full_price = int(
+                    item.select_one(
+                        "div.items-list > div > div > div > div.item__footer.mt-auto > div > div > div.price > a.store-price.fake-link"
+                    )
+                    .get_text()[:-2]
+                    .replace(" ", "")
                 )
-                .get_text()[:-2]
-                .replace(" ", "")
-            )
+            except AttributeError:
+                full_price = None
             sale_price = int(
-                soup.select_one("div.row div.online-price")
+                item.select_one(
+                    "div.items-list > div > div > div > div.item__footer.mt-auto > div > div > div.price > div.online-price"
+                )
                 .get_text()[:-2]
                 .replace(" ", "")
             )
             availability = (
-                soup.select_one(
-                    "div.pt-1.mt-1.pb-1 div.col-sm-8.m-auto div.btn:nth-child(1)"
+                item.select_one(
+                    "div.items-list > div > div > div > div.item__description > div.d-flex.justify-content-between > small.f-XS.d-inline-block.badge.badge-pill"
                 )
                 .get_text()
-                .strip()
+                .capitalize()
             )
-            if availability == "Купить":
-                availability = "Доступно"
-            else:
-                availability = "Под заказ"
+            parsed_item_data = {
+                "sku": sku,
+                "item_id": item_id,
+                "title": f"{title_base} {title_additional}",
+                "full_price": full_price,
+                "sale_price": sale_price,
+                "availability": availability,
+            }
+            parsed_page_data.append(parsed_item_data)
+        await write_to_db(parsed_page_data)
 
-            # parsed_page_data.update({
-            #     "title": title,
-            #     "SKU": sku,
-            #     "ID": id_,
-            #     "full_price": full_price,
-            #     "sale_price": sale_price,
-            #     "availability": availability
-            # })
-            # write_to_db(parsed_page_data)
-            parsed_page_data.append(
-                [title, sku, id_, full_price, sale_price, availability]
-            )
-            return
-        write_to_db(parsed_page_data)
+
+async def main() -> None:
+    session = requests.Session()
+    async with database:
+        await parse_data(session)
 
 
 if __name__ == "__main__":
-    session = requests.Session()
-    parse_data(session)
+    asyncio.run(main())
