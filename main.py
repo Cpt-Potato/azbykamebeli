c42b49b8cdf0c28fb8c67bb7f4fff4f7c9799865
Alexander Morozov <reverse43@gmail.com>
2022-07-05 14:52:16 +0300

    Add

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 
+from db.base import database
+
 app = FastAPI()
 
 
-# @app.on_event("startup")
-# async def startup():
-#     await database.connect()
-#
-#
-# @app.on_event("shutdown")
-# async def shutdown():
-#     await database.disconnect()
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/")
