370d4c0bf49630bea08f6b149f8edbbca6e179aa
Alexander Morozov <reverse43@gmail.com>
2022-07-05 14:52:09 +0300

    Rebase parser

---
+++ parser/app.py
@@ -0,0 +1,55 @@
+import asyncio
+from time import sleep
+
+import requests
+from bs4 import BeautifulSoup
+
+from db.base import database
+from db.models import straight_sofas
+from parser.base import get_availability, get_item_id, get_prices, get_sku, get_title
+
+base_url = "https://azbykamebeli.ru"
+url = "https://azbykamebeli.ru/catalog/0000057/"
+
+
+async def parse_and_save_data(session) -> None:
+    for page_number in range(17, 26):
+        print(f"Page {page_number}/25")
+        parsed_page_data = []
+        page_url = f"https://azbykamebeli.ru/catalog/0000057/?page={page_number}"
+        response = session.get(page_url)
+        soup = BeautifulSoup(response.text, "lxml")
+        items = soup.select("div.items-list > div > div > div")
+        sleep(2)
+
+        for item in items:
+            sku = get_sku(item)
+            item_id = get_item_id(item)
+            title = get_title(item)
+            full_price, sale_price = get_prices(item)
+            availability = get_availability(item)
+            parsed_item_data = {
+                "sku": sku,
+                "item_id": item_id,
+                "title": title,
+                "full_price": full_price,
+                "sale_price": sale_price,
+                "availability": availability,
+            }
+            parsed_page_data.append(parsed_item_data)
+        await write_to_db(parsed_page_data)
+
+
+async def write_to_db(data) -> None:
+    query = straight_sofas.insert()
+    await database.execute_many(query=query, values=data)
+
+
+async def main() -> None:
+    session = requests.Session()
+    async with database:
+        await parse_and_save_data(session)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- parser/base.py
+++ parser/base.py
@@ -1,43 +1,3 @@
-import asyncio
-import requests
-from bs4 import BeautifulSoup
-from time import sleep
-
-from db.base import database
-from db.models import straight_sofas
-
-base_url = "https://azbykamebeli.ru"
-url = "https://azbykamebeli.ru/catalog/0000057/"
-
-
-async def parse_and_save_data(session) -> None:
-    for page_number in range(17, 26):
-        print(f"Page {page_number}/25")
-        parsed_page_data = []
-        page_url = f"https://azbykamebeli.ru/catalog/0000057/?page={page_number}"
-        response = session.get(page_url)
-        soup = BeautifulSoup(response.text, "lxml")
-        items = soup.select("div.items-list > div > div > div")
-        sleep(2)
-
-        for item in items:
-            sku = get_sku(item)
-            item_id = get_item_id(item)
-            title = get_title(item)
-            full_price, sale_price = get_prices(item)
-            availability = get_availability(item)
-            parsed_item_data = {
-                "sku": sku,
-                "item_id": item_id,
-                "title": title,
-                "full_price": full_price,
-                "sale_price": sale_price,
-                "availability": availability,
-            }
-            parsed_page_data.append(parsed_item_data)
-        await write_to_db(parsed_page_data)
-
-
 def get_sku(item) -> str:
     return (
         item.select_one(
@@ -102,18 +62,3 @@ def get_availability(item):
         .get_text()
         .capitalize()
     )
-
-
-async def write_to_db(data) -> None:
-    query = straight_sofas.insert()
-    await database.execute_many(query=query, values=data)
-
-
-async def main() -> None:
-    session = requests.Session()
-    async with database:
-        await parse_and_save_data(session)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
